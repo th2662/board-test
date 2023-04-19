d623df371a0b6a5fc3fdfd57a52a515cdcb42a6a
stringT <th8260@naver.com>
2023-04-19 16:43:20 +0900

    #5 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    들어간 기술은
    * 스프링 웹
    * 액츄에이터
    * 데브툴즈
    * 롬복

---
+++ t-project-board/src/test/java/com/t/projectboard/TProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t.projectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TProjectBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
